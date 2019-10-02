400684409aed8b0ffab61dbd0833aed0991d8452
taransingh27 <taran.singh_cs18@gla.ac.in>
2019-10-02 20:59:57 +0530

    Complete code for question 2:Question_02.java

--- src/question02/Question_02.java
+++ src/question02/Question_02.java
@@ -1,4 +1,15 @@
 package question02;
 
+import java.util.Scanner;
+
 public class Question_02 {
+    public static void main(String[] args) {
+        System.out.println("Enter a String and indexes:");
+        Scanner obj = new Scanner(System.in);
+        String Str1 = obj.nextLine();
+        int lowerIndex = obj.nextInt();
+        int upperIndex = obj.nextInt();
+        System.out.println(Str1.substring(lowerIndex, upperIndex));
+    }
 }
+
