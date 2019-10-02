6c82b3f23088952663475b319ad40378af0dc984
taransingh27 <taran.singh_cs18@gla.ac.in>
2019-10-02 21:12:14 +0530

    code for question 6:Question_06.java

---
+++ src/question06/Question_06.java
@@ -0,0 +1,13 @@
+package question06;
+
+import java.util.Scanner;
+
+public class Question_06 {
+    public static void main(String[] args) {
+        System.out.println("Enter a String");
+        Scanner obj = new Scanner(System.in);
+        String Str1 = obj.nextLine();
+        System.out.println(Str1.toUpperCase());
+
+    }
+}
