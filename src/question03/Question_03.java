513c6a99cff0bda4b3fe683b2c461c284ec3053c
taransingh27 <taran.singh_cs18@gla.ac.in>
2019-10-02 21:06:21 +0530

    code for question 3:Question_03.java

--- src/question03/Question_03.java
+++ src/question03/Question_03.java
@@ -1,4 +1,15 @@
 package question03;
 
+import java.util.Scanner;
+
 public class Question_03 {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter a string and the character you want to replace and by which character");
+        String s1 = obj.nextLine();
+        char char1 = obj.next().charAt(0);
+        char char2 = obj.next().charAt(0);
+        s1 = s1.replace(char1, char2);
+        System.out.println(s1);
+    }
 }
